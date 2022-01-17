ad80e323db70abcbbc461619e9e90f44b33e307a
takonomura <takonomura@users.noreply.github.com>
2022-01-17 20:17:30 +0900

    Fix run_bench.sh

--- run_bench.sh
+++ run_bench.sh
@@ -3,4 +3,4 @@ set -e
 
 (cd benchmarker && bin/benchmarker -target "127.0.0.1:80" -exit-status)
 
-(cd webapp && docker-compose exec -T nginx cat /var/log/nginx/access2.log) | ./alp json --reverse --output=count,2xx,3xx,4xx,5xx,method,uri,min,p99,max --matching-groups="/api/schedules/[0-9A-Z]{26}"
+(cd webapp && docker-compose exec -T nginx cat /var/log/nginx/access2.log | ./alp json --reverse --output=count,2xx,3xx,4xx,5xx,method,uri,min,p99,max --matching-groups="/api/schedules/[0-9A-Z]{26}")
